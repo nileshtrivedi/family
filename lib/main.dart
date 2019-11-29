1068cb5ee8dc70b1bdf81d681839ec8e3ab2784a
nilesh <git@nileshtrivedi.com>
2019-11-29 23:44:19 +0530

    Added ChatScreen UI and CloudFirestore integration with test data fetch

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,94 +19,251 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
-      home: MyHomePage(title: 'Family Home Page'),
+      home: ChatDetails(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ChatDetails extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _ChatDetailsState createState() => _ChatDetailsState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _ChatDetailsState extends State<ChatDetails> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+        elevation: 0.4,
+        iconTheme: IconThemeData(color: Colors.black),
+        backgroundColor: Colors.white,
+        title: Row(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Container(
+              width: 40,
+              height: 40,
+              margin: EdgeInsets.fromLTRB(0, 5, 10, 0),
+              child: CircleAvatar(
+                backgroundImage: NetworkImage('https://i.pravatar.cc/105'),
+                backgroundColor: Colors.grey[200],
+                minRadius: 30,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                StreamBuilder(
+                  stream: Firestore.instance.collection('families').snapshots(),
+                  builder: (context, snapshot){
+                    if(!snapshot.hasData) return const Text('Loading...');
+                    return Text(
+                      snapshot.data.documents[0]['members'][0]['name'],
+                      style: TextStyle(color: Colors.black),
+                    );
+                  },
+                ),
+                Text(
+                  'Online Now',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 12,
+                  ),
+                )
+              ],
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Stack(
+        children: <Widget>[
+          Container(
+            color: Colors.white,
+            child: Column(
+              children: <Widget>[
+                Flexible(
+                  child: ListView.builder(
+                    itemCount: 1,
+                    shrinkWrap: true,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Column(
+                          children: <Widget>[
+                            Text(
+                              'Today',
+                              style:
+                              TextStyle(color: Colors.grey, fontSize: 12),
+                            ),
+                            Bubble(
+                              message: 'Hi How are you ?',
+                              isMe: true,
+                            ),
+                            Bubble(
+                              message: 'have you seen the docs yet?',
+                              isMe: true,
+                            ),
+                            Text(
+                              'Feb 25, 2018',
+                              style:
+                              TextStyle(color: Colors.grey, fontSize: 12),
+                            ),
+                            Bubble(
+                              message: 'i am fine !',
+                              isMe: false,
+                            ),
+                            Bubble(
+                              message: 'yes i\'ve seen the docs',
+                              isMe: false,
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            width: MediaQuery.of(context).size.width,
+            child: Container(
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                BoxShadow(
+                  color: Colors.grey[300],
+                  offset: Offset(-2, 0),
+                  blurRadius: 5,
+                ),
+              ]),
+              child: Row(
+                children: <Widget>[
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.camera,
+                      color: Color(0xff3E8DF3),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.image,
+                      color: Color(0xff3E8DF3),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 15),
+                  ),
+                  Expanded(
+                    child: TextFormField(
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                        hintText: 'Enter Message',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.send,
+                      color: Color(0xff3E8DF3),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Bubble extends StatelessWidget {
+  final bool isMe;
+  final String message;
+
+  Bubble({this.message, this.isMe});
+
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(5),
+      padding: isMe ? EdgeInsets.only(left: 40) : EdgeInsets.only(right: 40),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Column(
+            mainAxisAlignment:
+            isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+            crossAxisAlignment:
+            isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  gradient: isMe
+                      ? LinearGradient(
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomLeft,
+                      stops: [
+                        0.1,
+                        1
+                      ],
+                      colors: [
+                        Color(0xFFF6D365),
+                        Color(0xFFFDA085),
+                      ])
+                      : LinearGradient(
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomLeft,
+                      stops: [
+                        0.1,
+                        1
+                      ],
+                      colors: [
+                        Color(0xFFEBF5FC),
+                        Color(0xFFEBF5FC),
+                      ]),
+                  borderRadius: isMe
+                      ? BorderRadius.only(
+                    topRight: Radius.circular(15),
+                    topLeft: Radius.circular(15),
+                    bottomRight: Radius.circular(0),
+                    bottomLeft: Radius.circular(15),
+                  )
+                      : BorderRadius.only(
+                    topRight: Radius.circular(15),
+                    topLeft: Radius.circular(15),
+                    bottomRight: Radius.circular(15),
+                    bottomLeft: Radius.circular(0),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment:
+                  isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      message,
+                      textAlign: isMe ? TextAlign.end : TextAlign.start,
+                      style: TextStyle(
+                        color: isMe ? Colors.white : Colors.grey,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
